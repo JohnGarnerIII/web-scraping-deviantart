0f367e361f28ed8d7518a5ae28c73d87aec014df
John Garner <66663652+JohnGarnerIII@users.noreply.github.com>
2023-02-02 23:55:57 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+# send a GET request to the website
+response = requests.get("https://www.example.com/")
+
+# check if the request was successful
+if response.status_code == 200:
+    # parse the HTML content of the page
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # find all the elements you want to extract data from
+    elements = soup.find_all("element_tag", {"attribute_name": "attribute_value"})
+
+    # extract the data from the elements
+    for element in elements:
+        data = element.text
+        # process the data as needed
+
+# handle unsuccessful requests
+else:
+    print("Failed to fetch the page")
