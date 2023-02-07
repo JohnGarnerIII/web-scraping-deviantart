26b75ed13315a444fc3cae8c8d2ca3ffa5e14408
John Garner <66663652+JohnGarnerIII@users.noreply.github.com>
2023-02-07 03:32:50 -0500

    yeah

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 
-# send a GET request to the website
-response = requests.get("https://www.example.com/")
+# URL to the DeviantArt page you want to scrape
+url = "https://www.deviantart.com/yourusername/favourites"
 
-# check if the request was successful
+# Send a request to the URL and get the response
+response = requests.get(url)
+
+# Check if the request was successful
 if response.status_code == 200:
-    # parse the HTML content of the page
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # find all the elements you want to extract data from
-    elements = soup.find_all("element_tag", {"attribute_name": "attribute_value"})
-
-    # extract the data from the elements
-    for element in elements:
-        data = element.text
-        # process the data as needed
-
-# handle unsuccessful requests
-else:
-    print("Failed to fetch the page")
-
-
-#Testing new git commits with VSCode
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    # Find all the images on the page
+    images = soup.find_all("img")
+    
+    # Create a directory to store the images, if it doesn't already exist
+    directory = "deviantart_images"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        
+    # Loop through each image and download it
+    for image in images:
+        # Get the image URL
+        image_url = image.get("src")
+        
+        # Send a request to the image URL and get the response
+        image_response = requests.get(image_url)
+        
+        # Check if the request was successful
+        if image_response.status_code == 200:
+            # Get the image filename
+            image_filename = os.path.join(directory, os.path.basename(image_url))
+            
+            # Write the image data to the file
+            with open(image_filename, "wb") as f:
+                f.write(image_response.content)
+                
+print("Done!")
